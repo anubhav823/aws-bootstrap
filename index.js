cae1b85b8bb7b9e62cc1a0afc1570056295155ea
Anubhav <anubhav823@gmail.com>
2026-05-17 15:17:22 +0530

    added PM2 and basic server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const { hostname } = require('os');
+const msg = 'Hello world';
+const port = 8080;
+const server = http.createServer((req, res) =>{
+    res.statusCode = 200;
+    res.setHeader ('Content-Type', 'text/plain');
+    res.end(msg);
+});
+server.listen(port, hostname,  () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
